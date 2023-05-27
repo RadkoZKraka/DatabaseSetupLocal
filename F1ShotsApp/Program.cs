a7950d84170931ef24aeef0187183665df4fe251
Radosław Krowicki <rkkrowicki@gmail.com>
2023-05-27 14:20:22 +0200

    Session logout fix.

--- F1ShotsApp/Program.cs
+++ F1ShotsApp/Program.cs
@@ -84,7 +84,7 @@ builder.Services.ConfigureApplicationCookie(options =>
 {
     // Cookie settings
     options.Cookie.HttpOnly = true;
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
 
     options.LoginPath = "/Identity/Account/Logins";
     options.AccessDeniedPath = "/Identity/Account/AccessDenied";
